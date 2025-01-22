468df71f10d2fab972a73f760f67347f7dc502ef
Ashish Simon <ashishsimon25@gmail.com>
2025-01-23 00:09:53 +0530

    Chatio Commit

---
+++ Frontend/src/home/left_part/Left.jsx
@@ -0,0 +1,20 @@
+import Logout from "./Logout";
+import Search from "./Search";
+import Users from "./Users";
+
+function Left() {
+  return (
+    <div className="w-full bg-black text-gray-300">
+      <Search />
+      <div
+        className="flex-1 overflow-y-auto"
+        style={{ minHeight: "calc(84vh - 10vh)" }}
+      >
+        <Users />
+      </div>
+      <Logout />
+    </div>
+  );
+}
+
+export default Left;
